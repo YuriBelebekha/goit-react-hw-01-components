c129133e6476487e159f4ee3fe511729d2f46e61
Yuri Belebekha <ybelebekha@gmail.com>
2023-01-12 00:53:43 +0200

    Changed getRandomHexColor.js function

--- src/js/getRandomHexColor.js
+++ src/js/getRandomHexColor.js
@@ -1,7 +1,13 @@
 export { getRandomHexColor };
 
+// function returns only dark colors
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  let color = "#";
+  for (let i = 0; i < 3; i += 1) {
+    color += ("0" + Math.floor(Math.random() * Math.pow(16, 2) / 2)
+      .toString(16))
+      .slice(-2);
+  };
+  return color;
 };
